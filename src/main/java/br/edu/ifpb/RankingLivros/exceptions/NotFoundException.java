42ffa15bf33a5b8bcb7b48018abcb04f2ed1ce24
thalesluiz45 <thalesluiz45@hotmail.com>
2025-03-05 13:11:17 -0300

    General updates

--- src/main/java/br/edu/ifpb/RankingLivros/exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifpb.RankingLivros.exceptions;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException() {
-        super("Não foi possível encontrar o livro desejado.");
-    }
-}
